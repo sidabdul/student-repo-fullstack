3f8c320a4d46bb58800db017658bcc1b0225bf64
Sid-Ali Abdullahi <sidali@Sid-Alis-MacBook-Air.local>
2026-01-17 13:54:10 -0800

    restart

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -27,16 +27,5 @@ Use console.log() to write the proper output to the command line.
 // ...
 
 
-for (let j = 1; j <= 100; j++) 
-{
-  if (j % 3 === 0 && j % 5 === 0) {
-    console.log("fizzbuzz");
-  } else if (j % 3 === 0) {
-    console.log("fizz");
-  } else if (j % 5 === 0) {
-    console.log("buzz");
-  } else {
-    console.log(j);
-  }
-}
+
 
